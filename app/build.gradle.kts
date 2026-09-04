be101d5c6bb555adf2f44dbc76edc1b956e3ae43
djalpha814-png <djalpha814@gmail.com>
2026-09-05 00:31:01 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,21 @@
 plugins {
-    id("com.android.application") version "9.3.0" apply false
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.djalphatradingtrainer"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.djalphatradingtrainer"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
 }
